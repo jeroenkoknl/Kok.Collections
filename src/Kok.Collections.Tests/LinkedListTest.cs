a7843da0fa163cd9535827d0825997d9eb98d1d9
Jeroen Kok <info@jeroenkok.nl>
2016-01-07 16:09:16 +0100

    Initial commit

---
+++ src/Kok.Collections.Tests/LinkedListTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Kok.Collections;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Kok.Collections.Tests
+{
+    [TestFixture]
+    public class LinkedListTest
+    {
+        [Test]
+        public void AddTest()
+        {
+            LinkedList<int> list = new LinkedList<int> {1, 2, 3};
+            CollectionAssert.AreEqual(new [] { 1, 2, 3 }, list);
+        }
+
+        [Test]
+        public void AddFirstTest()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddFirst(3);
+            CollectionAssert.AreEqual(new [] { 3, 2, 1 }, list);
+        }
+
+        [Test]
+        public void RemoveTestMiddle()
+        {
+            LinkedList<int> list = new LinkedList<int> { 1, 2, 3 };
+            list.Remove(2);
+            CollectionAssert.AreEqual(new [] { 1, 3 }, list);
+        }
+
+        [Test]
+        public void RemoveTestLast()
+        {
+            LinkedList<int> list = new LinkedList<int> { 1, 2, 3 };
+            list.Remove(3);
+            CollectionAssert.AreEqual(new[] { 1, 2 }, list);
+        }
+
+        [Test]
+        public void RemoveTestFirst()
+        {
+            LinkedList<int> list = new LinkedList<int> { 1, 2, 3 };
+            list.Remove(1);
+            CollectionAssert.AreEqual(new[] { 2, 3 }, list);
+        }
+
+        [Test]
+        public void RemoveLastAndAdd()
+        {
+            LinkedList<int> list = new LinkedList<int> { 1, 2, 3 };
+            list.Remove(3);
+            list.Add(4);
+            CollectionAssert.AreEqual(new[] { 1, 2, 4 }, list);
+        }
+
+        [Test]
+        public void RemoveFirstAndAdd()
+        {
+            LinkedList<int> list = new LinkedList<int> { 1, 2, 3 };
+            list.Remove(1);
+            list.AddFirst(0);
+            CollectionAssert.AreEqual(new[] { 0, 2, 3 }, list);
+        }
+    }
+}
---
+++ src/Kok.Collections/LinkedList.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kok.Collections
+{
+    public class LinkedList<T> : ICollection<T>
+    {
+        private Node first;
+        private Node last;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Nodes.Select(node => node.Value).GetEnumerator();
+        }
+
+        private IEnumerable<Node> Nodes
+        {
+            get
+            {
+                var current = first;
+                while (current != null)
+                {
+                    yield return current;
+                    current = current.Next;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            AddLast(item);
+        }
+
+        public void AddFirst(T item)
+        {
+            var next = first;
+            first = new Node(item, null, first);
+            if (next == null)
+            {
+                last = first;
+            }
+            else
+            {
+                next.Previous = first;
+            }
+        }
+
+        public void Clear()
+        {
+            first = null;
+        }
+
+        public bool Contains(T item)
+        {
+            return Nodes.Any(node => Equals(item, node.Value));
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+            if (arrayIndex < 0 || arrayIndex >= array.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            }
+            var enumerator = GetEnumerator();
+            for (var i = arrayIndex; i < array.Length && enumerator.MoveNext(); i++)
+            {
+                array[i] = enumerator.Current;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            Node previous = null;
+            var current = first;
+            while (current != null)
+            {
+                var next = current.Next;
+                if (Equals(item, current.Value))
+                {
+                    if (previous == null)
+                    {
+                        first = current.Next;
+                    }
+                    else
+                    {
+                        previous.Next = next;
+                        if (next == null)
+                        {
+                            last = previous;
+                        }
+                        else
+                        {
+                            next.Previous = previous;
+                        }
+                    }
+                    return true;
+                }
+                previous = current;
+                current = next;
+            }
+            return false;
+        }
+
+        public int Count => Nodes.Count();
+
+        public bool IsReadOnly => false;
+
+        public void AddLast(T item)
+        {
+            var previous = last;
+            last = new Node(item, last, null);
+            if (previous == null)
+            {
+                first = last;
+            }
+            else
+            {
+                previous.Next = last;
+            }
+        }
+
+        private class Node
+        {
+            public Node(T value, Node previous, Node next)
+            {
+                Previous = previous;
+                Next = next;
+                Value = value;
+            }
+
+            public Node Next { get; set; }
+
+            public Node Previous { get; set; }
+            public T Value { get; }
+        }
+    }
+}
